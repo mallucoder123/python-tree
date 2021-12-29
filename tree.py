c9c199b7b1c67dc20a3234a91f586e15963f89bd
mallucoder123 <96638108+mallucoder123@users.noreply.github.com>
2021-12-29 19:42:58 +0530

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,7 @@
+fav = int(input("PICK ARANDOM NUMBER FROM 3 - 10"))
+i=1
+for i in range(fav):
+    print("  "*(fav-i)+"*   "*i)
+for j in range(2):
+    print("       "+"     |"*3)
+print("<____MERRY CHRISTMAS___>")
